590c9a78074ed6c72b3ca08ad4b23bed0a1c8123
Camilo Mejia Monsalve <camilo.mon96@gmail.com>
2020-08-07 16:46:39 -0500

    creating the table element

---
+++ js/Tablely.js
@@ -0,0 +1,20 @@
+function Tablely(elementId, data, inputs = [5, 10, 15, 20]) {
+    this.boxElement = document.getElementById(elementId);
+    this.headers = Object.keys(data[0]);
+    this.totalRowsToShow = data.length;
+    this.indexOfRowsToDisplay = [];
+    this.rowsPerPage = 5;
+    this.currentPage = [0, 1];
+    this.pagesNumber = Math.ceil((this.indexOfRowsToDisplay.length > 0 ? this.indexOfRowsToDisplay.length : this.totalRowsToShow) / this.rowsPerPage);
+
+
+    this.createTable = () => {
+        let table = document.createElement('table');
+        table.setAttribute('id', 'tablely_table_' + elementId)
+        return table;
+    }
+
+   
+
+    
+}
